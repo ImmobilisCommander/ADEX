3d278ee47529d2084c93de66c265f4ecab43c785
Julien <julien_lefevre@outlook.fr>
2020-05-21 12:08:21 +0200

    First commit of solution

---
+++ Adex/Adex.App/Program.cs
@@ -0,0 +1,43 @@
+﻿using Adex.Model;
+using System.IO;
+using System.Text;
+
+namespace Adex.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AdexContext())
+            {
+                db.Companies.Add(new Company() { Designation = "test" });
+                db.SaveChanges();
+            }
+        }
+
+        private static void GenerateSmallDataSample()
+        {
+            var files = Directory.GetFiles(@"E:\Git\ImmobilisCommander\ADEX\exports-etalab", "*.csv");
+            var sb = new StringBuilder();
+
+            foreach (var f in files)
+            {
+                using (var r = new StreamReader(f, true))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        sb.Append($"{r.ReadLine()}\n");
+                    }
+                }
+
+                var newFile = Path.Combine(@"E:\Git\ImmobilisCommander\ADEX\Data", Path.GetFileName(f));
+                if (File.Exists(newFile))
+                {
+                    File.Delete(newFile);
+                }
+                File.WriteAllText(newFile, sb.ToString(), Encoding.UTF8);
+                sb.Clear();
+            }
+        }
+    }
+}
---
+++ Adex/Adex.Model/AdexContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Adex.Model
+{
+    public class AdexContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Adex;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class Entity
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// External identifier
+        /// </summary>
+        [MaxLength(200)]
+        public string ExternalId { get; set; }
+    }
+
+    public class Company : Entity
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [MaxLength(200)]
+        public string Designation { get; set; }
+    }
+
+    public class Beneficiary : Entity
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [MaxLength(200)]
+        public string LastName { get; set; }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [MaxLength(200)]
+        public string FirstName { get; set; }
+    }
+}
